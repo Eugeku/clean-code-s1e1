927e0e28d7b2396fe6926ce023e4e95319906c11
Eugeku <eugenekuzora@gmail.com>
2024-12-16 13:21:12 +0300

    refactor: add media alternatives according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete-button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-image";
-    deleteButtonImg.alt="delete-image"
+    deleteButtonImg.alt="Delete image"
     deleteButton.appendChild(deleteButtonImg);
 
 
